0131d56e1486e7781319557dba2f3851eac9aa72
NSagan271 <naomi.sagan@gmail.com>
2015-03-30 19:02:36 -0700

    Removed Junk
    Removed theme-related junk code.

--- SuggestApp/app/src/main/java/piratecrew/suggestapp/About.java
+++ SuggestApp/app/src/main/java/piratecrew/suggestapp/About.java
@@ -7,64 +7,32 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
-
-import static android.graphics.Color.BLACK;
-import static android.graphics.Color.BLUE;
-import static android.graphics.Color.GREEN;
-import static android.graphics.Color.RED;
-import static android.graphics.Color.WHITE;
-import static android.graphics.Color.YELLOW;
 
 
 public class About extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        switch (Themes.t){
+            case 1:{
+                setTheme(android.R.style.Theme_Holo_NoActionBar);
+                break;
+            }
+            case 2:{
+                setTheme(android.R.style.Theme_DeviceDefault_Light_NoActionBar);
+                break;
+            }
+            case 3:{
+                setTheme(android.R.style.Theme_DeviceDefault_Wallpaper_NoTitleBar);
+                break;
+            }
+            default: setTheme(android.R.style.Theme_DeviceDefault_NoActionBar);
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
         //buttons,text
         Button toSettings3 = (Button) findViewById(R.id.toSettings3);
-        final RelativeLayout about1 = (RelativeLayout) findViewById(R.id.about1);
-        final TextView abouttitle = (TextView) findViewById(R.id.abouttitle);
-        final TextView atext = (TextView) findViewById(R.id.atext);
-        //setting colors
-        if (Themes.t == 0){
-            about1.setBackgroundColor(WHITE);
-            abouttitle.setTextColor(BLACK);
-            atext.setTextColor(BLACK);
-        }
-        else if (Themes.t == 1){
-            about1.setBackgroundColor(GREEN);
-            abouttitle.setTextColor(BLACK);
-            atext.setTextColor(BLACK);
-        }
-        else if (Themes.t == 2){
-            about1.setBackgroundColor(BLUE);
-            abouttitle.setTextColor(WHITE);
-            atext.setTextColor(WHITE);
-        }
-        else if (Themes.t == 3){
-            about1.setBackgroundColor(RED);
-            abouttitle.setTextColor(WHITE);
-            atext.setTextColor(WHITE);
-        }
-        else if (Themes.t == 4){
-            about1.setBackgroundColor(YELLOW);
-            abouttitle.setTextColor(BLACK);
-            atext.setTextColor(BLACK);
-        }
-        else if (Themes.t == 5){
-            about1.setBackgroundColor(BLACK);
-            abouttitle.setTextColor(WHITE);
-            atext.setTextColor(WHITE);
-        }
-        else{
-            about1.setBackgroundColor(WHITE);
-            abouttitle.setTextColor(BLACK);
-            atext.setTextColor(BLACK);
-        }
+
         //back to settings
         toSettings3.setOnClickListener(new View.OnClickListener() {
             @Override
