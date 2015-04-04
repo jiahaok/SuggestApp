b8a876f5dc73c3d94e16db19195f715be3e76136
mydogisjibe <mydogsijibe@gmail.com>
2015-04-04 16:39:27 -0700

    Async DB
    Database uses AsyncTask

--- SuggestApp/app/src/main/java/piratecrew/suggestapp/DatabaseConnection.java
+++ SuggestApp/app/src/main/java/piratecrew/suggestapp/DatabaseConnection.java
@@ -9,6 +9,7 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 
+import android.os.AsyncTask;
 import android.os.StrictMode;
 import android.widget.TextView;
 
@@ -27,50 +28,61 @@ import java.net.URI;
  */
 public class DatabaseConnection {
     private final String WEB_ROOT = "http://10.0.0.4";
-
+    TextView text;
     DatabaseConnection(TextView textView){
-
+        text = textView;
         if (android.os.Build.VERSION.SDK_INT > 9) {
             StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
             StrictMode.setThreadPolicy(policy);
         }
-        String dataAsString = null;
-        try {
-            //Get the response
-            HttpClient client = new DefaultHttpClient();
-            HttpGet request = new HttpGet();
-            request.setURI(new URI(WEB_ROOT+"?id=1"));
-            HttpResponse response = client.execute(request);
 
-            //Get content of response
-            HttpEntity  entity = response.getEntity();
-            InputStream inputStream = entity.getContent();
+        new SentGetRequest().execute(WEB_ROOT+"?id=1");
 
-            ByteArrayOutputStream content = new ByteArrayOutputStream();
+        textView.setText("Loading...");
+    }
 
-            // Read response into a buffered stream
-            int readBytes = 0;
-            byte[] sBuffer = new byte[512];
-            while((readBytes = inputStream.read(sBuffer)) != -1) {
-                content.write(sBuffer, 0, readBytes);
+    //get
+    private class SentGetRequest extends AsyncTask<String, Void, String> {
+        protected String doInBackground(String... params) {
+            try {
+                //Get the response
+                HttpClient client = new DefaultHttpClient();
+                HttpGet request = new HttpGet();
+                request.setURI(new URI(params[0]));
+                HttpResponse response = client.execute(request);
+
+                //Get content of response
+                HttpEntity  entity = response.getEntity();
+                InputStream inputStream = entity.getContent();
+
+                ByteArrayOutputStream content = new ByteArrayOutputStream();
+
+                // Read response into a buffered stream
+                int readBytes = 0;
+                byte[] sBuffer = new byte[512];
+                while((readBytes = inputStream.read(sBuffer)) != -1) {
+                    content.write(sBuffer, 0, readBytes);
+                }
+
+                //Return result from buffered stream
+                return new String(content.toByteArray());
+
+
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            } catch (ClientProtocolException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
             }
-
-            //Return result from buffered stream
-            dataAsString = new String(content.toByteArray());
-
-
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        } catch (ClientProtocolException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            return null; //If program gets this far, something didn't work.
         }
-        if(!(dataAsString == null)) {
-            textView.setText(dataAsString);
+
+        protected void onPostExecute(String result){
+            text.setText(result);
+
         }
-        else textView.setText("it didn't work.....");
     }
 }
