2c43e3bbd31bbd644ce5678d1a09a454a4e8f9de
mydogisjibe <mydogsijibe@gmail.com>
2015-04-07 22:12:39 -0700

    POST code added; Server swap
    POST now functional. Some guy gave us a server to use. WEB_ROOT has been set as "brentluker.com"

--- SuggestApp/app/src/main/java/piratecrew/suggestapp/DatabaseConnection.java
+++ SuggestApp/app/src/main/java/piratecrew/suggestapp/DatabaseConnection.java
@@ -15,27 +15,37 @@ import android.widget.TextView;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Brent on 3/30/2015.
  */
 public class DatabaseConnection {
-    private final String WEB_ROOT = "http://10.0.0.4/";
+    private final String WEB_ROOT = "http://www.brentluker.com/";
     TextView text;
     DatabaseConnection(String username, String password, TextView textView){
         if (android.os.Build.VERSION.SDK_INT > 9) {
             StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
             StrictMode.setThreadPolicy(policy);
-            text = textView;
         }
-        new SendGetRequest().execute(WEB_ROOT+"login.php?username="+username+"&password="+password);
+        text = textView;
+        String[] site = {WEB_ROOT+"login.php"};
+        String[] data1 = {"username", username};
+        String[] data2 = {"password", password};
+        new SendPostRequest().execute(site, data1, data2);
     }
     /**
      * Tests how to make a GET request.
@@ -51,6 +61,20 @@ public class DatabaseConnection {
         new SendGetRequest().execute(WEB_ROOT+"?id=1");
 
         textView.setText("Loading...");
+    }
+    void postTest(TextView textView){
+        text = textView;
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+
+        textView.setText("Loading...");
+        String[] strings = {WEB_ROOT};
+        String[] strings1= {"id", "1"};
+        new SendPostRequest().execute(strings, strings1);
+
+
     }
 
     //get
@@ -97,5 +121,60 @@ public class DatabaseConnection {
 
         }
     }
+    private class SendPostRequest extends AsyncTask<String[], Void, String> {
 
+        protected String doInBackground(String[]... params) {
+            try {
+                //Create request
+                HttpClient client = new DefaultHttpClient();
+                HttpPost httpPost = new HttpPost();
+
+                httpPost.setURI(new URI(params[0][0]));
+
+
+                List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(params.length-1);
+                for(int i = 1; i < params.length; i++) {
+                    nameValuePairs.add(new BasicNameValuePair(params[i][0], params[i][1]));
+                }
+                httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+                //Get the response
+                HttpResponse response = client.execute(httpPost);
+
+                //Get content of response
+                HttpEntity  entity = response.getEntity();
+                InputStream inputStream = entity.getContent();
+
+                ByteArrayOutputStream content = new ByteArrayOutputStream();
+
+                // Read response into a buffered stream
+                int readBytes = 0;
+                byte[] sBuffer = new byte[512];
+                while((readBytes = inputStream.read(sBuffer)) != -1) {
+                    content.write(sBuffer, 0, readBytes);
+                }
+
+                //Return result from buffered stream
+                return new String(content.toByteArray());
+
+
+            } catch (URISyntaxException e ){
+                // TODO Generic catch
+                e.printStackTrace();
+            }
+            catch (ClientProtocolException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            return null; //If program gets this far, something didn't work.
+        }
+
+        protected void onPostExecute(String result){
+            text.setText(result);
+
+        }
+    }
 }
--- SuggestApp/app/src/main/java/piratecrew/suggestapp/LoginActivity.java
+++ SuggestApp/app/src/main/java/piratecrew/suggestapp/LoginActivity.java
@@ -38,6 +38,8 @@ public class LoginActivity extends MainActivity {
                 DatabaseConnection db = new DatabaseConnection(usernameField, passwordField, status);
             }
         });
+
+
     }
 
 
