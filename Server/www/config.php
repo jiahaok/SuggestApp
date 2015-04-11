a90149407db46da05c93dc7334da781c6f4ee96c
mydogisjibe <mydogsijibe@gmail.com>
2015-04-11 14:14:16 -0700

    Create Functionality
    Add abillity to create questions, every part works exept saving bitmaps. Add page to display all questions, since sometimes the phpmyadmin page was getting slow.

---
+++ Server/www/config.php
@@ -0,0 +1,5 @@
+<?php
+define("DATABASE_HOST", "dc2-mysql-03.kattare.com");
+define("DATABASE_USERNAME", "mydogisjibe");
+define("DATABASE_PASSWORD", "db-turtlevc42");
+define("DATABASE_NAME", "mydogisjibe");
