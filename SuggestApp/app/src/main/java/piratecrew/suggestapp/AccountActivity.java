fbff24c830c52f20a2e5bea385d3a952742f5e29
xeonjake <jiahaokuang7066@gmail.com>
2015-04-13 21:47:37 -0700

    nightly commit 4/13/15
    added some checking

--- SuggestApp/app/src/main/java/piratecrew/suggestapp/AccountActivity.java
+++ SuggestApp/app/src/main/java/piratecrew/suggestapp/AccountActivity.java
@@ -11,6 +11,9 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 public class AccountActivity extends ActionBarActivity {
 
@@ -25,34 +28,81 @@ public class AccountActivity extends ActionBarActivity {
                 startActivity(intent);
             }
         });
-
+        //start local variable declaration
+         boolean emailIsUsed = true;
+        //end local variable declaration
 
         findViewById(R.id.signup).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String username  = ((EditText) findViewById(R.id.email   )).getText().toString();
-                String checkUser = ((EditText) findViewById(R.id.rePassword)).getText().toString();
+                String rPassword = ((EditText) findViewById(R.id.rePassword)).getText().toString();
                 String password  = ((EditText) findViewById(R.id.password)).getText().toString();
                 TextView status = (TextView) findViewById(R.id.status);
 
+                for(short x = 0; x<=1; x++) {
 
-                if(username.equals("") || password.equals(""))
-                    status.setText("Must have username and password");
-                else if(!username.equals(checkUser))
-                    status.setText("Email doesn't match");
-                else if(!((RadioButton) findViewById(R.id.radioButton)).isChecked())
-                    status.setText("Must agree to terms of use");
-                //TODO: else create user
+                    if (username.equals(null)) {//check if email field is empty
+                        status.setText("Please enter your Email");
+                        break;
+                    }
+                    else if (!validEmail(username)){// check the email format
+                        status.setText("Email is not valid");
+                    }
+                    else if (!serverEmailCheckUsed()) {//check if email is used
+                        status.setText("Email have already been used");
+                        break;
+                    }
+                    else if (password.equals(null) || password.length() < 6) {//check if password is empty or less than 6 character
+                        status.setText("Please choose a password 6 character or longer");
+                        break;
+                    }
+                    else if (password.equalsIgnoreCase(rPassword)) {//check retyped password
+                        status.setText("Password does not match");
+                        break;
+                    }
+                    else if (!((RadioButton) findViewById(R.id.radioButton)).isChecked()) {//check term of use agreement
+                        status.setText("Must agree to terms of use");
+                        break;
+                    }
 
+                    compressAndSend(username,password);
+
+                }
 
             }
+
         });
-
-
-
 
     }
 
+    public void compressAndSend(String username, String password){
+        //Todo: submission code
+    }
+
+    public boolean validEmail(String username) {
+        boolean valid;
+
+        String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$"; // regex code for email format(i hate this)
+        CharSequence usernameIn = username;
+        //check format
+        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(usernameIn);
+        if (matcher.matches()) {
+            valid = true;
+        }
+        else{
+            valid = false;
+        }
+
+        return valid;
+    }
+
+    public boolean serverEmailCheckUsed(){ //check if email is used
+        boolean serverEmailCheckBooleanReturn = false;// true = not used, false = used
+        //Todo: check email with server, also remove ^^ line
+        return serverEmailCheckBooleanReturn;
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
