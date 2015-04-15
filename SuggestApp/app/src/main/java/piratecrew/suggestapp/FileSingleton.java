4dddf6a453eec6ef574faf5b416fec4c6ae2361d
NSagan271 <naomi.sagan@gmail.com>
2015-04-15 09:02:33 -0700

    Fixed Singleton Class
    Made singleton class file code work.

--- SuggestApp/app/src/main/java/piratecrew/suggestapp/FileSingleton.java
+++ SuggestApp/app/src/main/java/piratecrew/suggestapp/FileSingleton.java
@@ -2,8 +2,11 @@ package piratecrew.suggestapp;
 
 import android.util.Log;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * Created by Naomi on 4/13/15.
@@ -21,12 +24,12 @@ public class FileSingleton {
 
     public static void writeFile(String data, String file){
         try {
-            FileOutputStream fOut = new FileOutputStream(file);
+            FileOutputStream fOut = new FileOutputStream(new File("/data/data/piratecrew.suggestapp/"+file+".txt"),false);
             fOut.write(data.getBytes());
             fOut.close();
             Log.i("Write", data);
 
-        } catch(Exception e) {
+        }catch(Exception e) {
             Log.e("Error", Log.getStackTraceString(e));
         }
     }
@@ -34,7 +37,7 @@ public class FileSingleton {
         int c;
         String temp = "";
         try {
-            FileInputStream fin = new FileInputStream(file);
+            FileInputStream fin = new FileInputStream(new File("/data/data/piratecrew.suggestapp/"+file+".txt"));
             while ((c = fin.read()) != -1) {
                 temp = temp + Character.toString((char) c);
             }
